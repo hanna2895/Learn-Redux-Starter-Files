cad27bd3c47ff6b7a7719f7753e1908ae9971901
Hannah Werman <hanna.w28@gmail.com>
2018-05-24 12:22:30 -0500

    added handlesubmit function to comment form

--- learn-redux/client/actions/actionCreators.js
+++ learn-redux/client/actions/actionCreators.js
@@ -7,6 +7,7 @@ export function increment(index) {
 }
 // add comments
 export function addComment(postId, author, comment) {
+  console.log('dispatching add comment')
   return {
     type: "ADD_COMMENT",
     postId,
