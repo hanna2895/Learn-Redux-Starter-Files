01786ee45b9c5754780cbccfe79624f90c9581df
Hannah Werman <hanna.w28@gmail.com>
2018-05-24 12:04:23 -0500

    layout for single photo component, created comment component

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router'
+import { Link } from 'react-router';
+import Photo from './photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
+    // index of the post
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    console.log(i);
+    // get us the post
+    const post = this.props.posts[i];
+    console.log(post);
+
     return (
       <div className="single-photo">
-        Im the single photo
+        <Photo i={i} post={post} {...this.props} />
+        <Comments />
       </div>
     )
   }
--- learn-redux/client/components/photo.js
+++ learn-redux/client/components/photo.js
@@ -8,8 +8,8 @@ const Photo = React.createClass({
     return (
       <figure className="grid-figure">
         <div className="grid-photo-wrap">
-          <Link to={'/view/${post.code}'}>
-            <img src={post.display_src} alt={post.caption} className="grid-photo"/>
+          <Link to={`/view/${post.code}`}>
+            <img src={post.display_src} alt={post.caption} className="grid-photo" />
           </Link>
 
           <CSSTransitionGroup transitionName="like" transitionEnterTimeout={500} transitionLeaveTimeout={500}>
@@ -22,7 +22,7 @@ const Photo = React.createClass({
           <p>{post.caption}</p>
           <div className="control-buttons">
             <button onClick={this.props.increment.bind(null, i)} className="likes">&hearts; {post.likes} </button>
-            <Link className="button" to={'/view/${post.code}'}>
+            <Link className="button" to={`/view/${post.code}`}>
               <span className="comment-count">
                 <span className="speech-bubble"></span>
                 {comments[post.code] ? comments[post.code].length : 0}
