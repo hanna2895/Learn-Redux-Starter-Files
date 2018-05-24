831be77f2f3799a5bcc4202bc5a266025862ee95
Hannah Werman <hanna.w28@gmail.com>
2018-05-24 12:37:38 -0500

    added postComments function to comments reducer to add and delete comments

--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -1,4 +1,35 @@
+function postComments(state = [], action) {
+  switch(action.type) {
+    case 'ADD_COMMENT' :
+      // return existing state plus the new comment
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }];
+    case 'REMOVE_COMMENT' :
+      console.log('removing a comment')
+      // we need to return state without the deleted comment
+      return [
+        // from the start to the one we want to delete
+        ...state.slice(0, action.i),
+        // after the deleted one
+        ...state.slice(action.i + 1)
+      ]
+    default:
+      return state;
+  }
+  return state;
+}
+
 function comments(state = [], action) {
+  if(typeof action.postId !== 'undefined') {
+    return {
+      // take the current state
+      ...state,
+      // overwrite this post with a new one
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
   return state;
 }
 
